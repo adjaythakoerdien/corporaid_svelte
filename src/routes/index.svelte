187f880c89eca7211f54464d760e78e9b1131c5a
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-17 10:14:51 +0200

    Init with prev website

---
+++ src/routes/index.svelte
@@ -0,0 +1,73 @@
+<style>
+@import "index.css";
+</style>
+
+<script>
+    import pijl from '../icons/arrow_up.png';
+</script>
+
+
+<nav class="blue-grey lighten-3">
+    <div class="nav-wrapper blue-grey lighten-3">
+        <a href="#" class="brand-logo black-text">CorporAid</a>
+        <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons" style="color:#323E66;">menu</i></a>
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#" class="black-text">Over ons</a></li>
+            <li><a href="#" class="black-text">Onze diensten</a></li>
+            <li><a href="#" class="black-text">Klant cases</a></li>
+            <li><a href="#" class="waves-effect waves-light btn z-depth-0 deep-orange accent-2" style="border-radius:34px;">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
+<ul class="sidenav" id="slide-out">
+    <li><a href="#" class="black-text">Over ons</a></li>
+    <li><a href="#" class="black-text">Onze diensten</a></li>
+    <li><a href="#" class="black-text">Klant cases</a></li>
+    <li><a href="#" class="waves-effect waves-light btn z-depth-0 deep-orange accent-2" style="border-radius:34px;">Contact</a></li>
+</ul>
+
+
+
+
+<main style="height:722px;width:100%;background-color:#323E66;">
+    <section class="hero-section">
+        <div class="row">
+            <div class="hero-image col m6 hide-on-small-only">
+
+            </div>
+            <div class="hero-container col s12 m4">
+
+                <div class="hero-title hero-text white-text right">
+                    FRESH FINANCE PERSPECTIVE
+                </div>
+
+                <div class="hero-subtitle hero-text right">
+                    Op zoek naar interim controller of CFO professionals?
+                    Wij brengen je finance naar
+                    <span class="the-next-level">
+                        the next level
+                    </span>
+                </div>
+                <div class="right" style="width:400px;">
+                    <div>
+                        <a href="#" class="hero-button right hero-text waves-effect waves-light btn z-depth-0 deep-orange accent-2">NEEM DIRECT CONTACT OP</a>
+                    </div>
+
+                    <div class="right white-text hero-button-subtext arrow">
+                        <img src={pijl} alt="">
+                    </div>
+
+                    <div class="right white-text hero-button-subtext">
+                        Kennismaken is altijd GRATIS
+                    </div>
+                </div>
+
+            </div>
+            <div class="hero-image beneden col s12 hide-on-med-and-up" style="background-color: #323E66">
+
+            </div>
+        </div>
+    </section>
+</main>
