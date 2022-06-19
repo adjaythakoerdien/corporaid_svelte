9fdb47a16cbb222e986f5afdd46147daccf61be3
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-19 11:20:53 +0200

    Update header links for home and contact

--- src/lib/layout/header.svelte
+++ src/lib/layout/header.svelte
@@ -1,13 +1,13 @@
 <nav class="blue-grey lighten-3">
     <div class="nav-wrapper blue-grey lighten-3">
-        <a href="#" class="brand-logo black-text">Corpor<span class="sidenav-logo">Aid</span></a>
+        <a href="/" class="brand-logo black-text">Corpor<span class="sidenav-logo">Aid</span></a>
         <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons" style="color:#323E66;">menu</i></a>
 
         <ul id="nav-mobile" class="right hide-on-med-and-down header-links">
             <li><a href="#" class="black-text">Over ons</a></li>
             <li><a href="#" class="black-text">Onze diensten</a></li>
             <li><a href="#" class="black-text">Klant cases</a></li>
-            <li><a href="#" class="waves-effect waves-light btn hoverable deep-orange accent-2" style="border-radius:34px;">Contact</a></li>
+            <li><a href="/contact" class="waves-effect waves-light btn hoverable deep-orange accent-2" style="border-radius:34px;">Contact</a></li>
         </ul>
     </div>
 </nav>
@@ -20,5 +20,5 @@
     <li><a href="#" class="black-text">Over ons</a></li>
     <li><a href="#" class="black-text">Onze diensten</a></li>
     <li><a href="#" class="black-text">Klant cases</a></li>
-    <li><a href="#" class="waves-effect waves-light btn hoverable deep-orange accent-2" style="border-radius:34px;">Contact</a></li>
+    <li><a href="/contact" class="waves-effect waves-light btn hoverable deep-orange accent-2 sidenav-close" style="border-radius:34px;">Contact</a></li>
 </ul>
