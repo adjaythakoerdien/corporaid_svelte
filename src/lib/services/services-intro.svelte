cd40051d4a1b223590595cf44a1c9a5de678e8d7
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-26 16:58:51 +0200

    Styling

--- src/lib/services/services-intro.svelte
+++ src/lib/services/services-intro.svelte
@@ -4,7 +4,7 @@
 
 <section class="">
 	<article class="container center">
-		<h1 class="intro-title">
+		<h1 class="intro-title services-section intro">
 			{$_('services.intro.title')}<span class="h1-span">{$_('services.intro.titleHighlight')}</span>
 		</h1>
 		<p class="services-intro-text">
