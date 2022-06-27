b0cea7caa3249b302df00f063c54d69ef989cd8f
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-27 09:35:02 +0200

    Updated logo's alt text

--- src/lib/home/home-carousel.svelte
+++ src/lib/home/home-carousel.svelte
@@ -6,14 +6,18 @@
 	<article class="container carousel-container center">
 		<h2 class="carousel-title">{$_('home.logosSectie.title')}</h2>
 		<div class="logos-container" role="presentation">
-			<img class="carousel-logo" src="/logos/nyerode.webp" alt="" />
-			<img class="carousel-logo" src="/logos/dgb.webp" alt="" />
-			<img class="carousel-logo" src="/logos/kroonenberg.webp" alt="" />
-			<img class="carousel-logo" src="/logos/rappange.webp" alt="" />
-			<img class="carousel-logo" src="/logos/crop.webp" alt="" />
-			<img class="carousel-logo" src="/logos/museum-kaart.webp" alt="" />
-			<img class="carousel-logo" src="/logos/museum-vereniging.webp" alt="" />
-			<img class="carousel-logo" src="/logos/i-love-sushi.webp" alt="" />
+			<img class="carousel-logo" src="/logos/nyerode.webp" alt="Nyenrode logo" />
+			<img class="carousel-logo" src="/logos/dgb.webp" alt="DGB Group logo" />
+			<img class="carousel-logo" src="/logos/kroonenberg.webp" alt="Kroonenberg Groep logo" />
+			<img
+				class="carousel-logo"
+				src="/logos/rappange.webp"
+				alt="Rappange & Partners Architecten logo"
+			/>
+			<img class="carousel-logo" src="/logos/crop.webp" alt="CROP logo" />
+			<img class="carousel-logo" src="/logos/museum-kaart.webp" alt="Museum Kaart logo" />
+			<img class="carousel-logo" src="/logos/museum-vereniging.webp" alt="Museum Vereniging logo" />
+			<img class="carousel-logo" src="/logos/i-love-sushi.webp" alt="I Love Sushi logo" />
 		</div>
 		<p class="carousel-subtitle">
 			{$_('home.logosSectie.subtitle')}
