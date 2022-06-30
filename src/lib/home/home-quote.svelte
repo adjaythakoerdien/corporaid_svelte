e73cd437f993550e6c1e756bded62a55eff087a0
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-30 10:34:19 +0200

    Temp removed quote section

--- src/lib/home/home-quote.svelte
+++ src/lib/home/home-quote.svelte
@@ -11,19 +11,19 @@
 			<i class="material-icons white-text" style="font-size:20px;">star</i>
 			<i class="material-icons white-text" style="font-size:20px;">star</i>
 		</div>
-		<p class="quote-text">
-			{$_('home.quote.text')}
-		</p>
-		<div class="quote-person-container" role="presentation">
-			<div class="quote-photo">
-				<img src="/logos/avatar.webp" alt="" />
-			</div>
-			<div class="quote-person" role="presentation">
-				{$_('home.quote.name')}
-				<br />
-				{$_('home.quote.company')}
-			</div>
-		</div>
+		<!--		<p class="quote-text">-->
+		<!--			{$_('home.quote.text')}-->
+		<!--		</p>-->
+		<!--		<div class="quote-person-container" role="presentation">-->
+		<!--			<div class="quote-photo">-->
+		<!--				<img src="/logos/avatar.webp" alt="" />-->
+		<!--			</div>-->
+		<!--			<div class="quote-person" role="presentation">-->
+		<!--				{$_('home.quote.name')}-->
+		<!--				<br />-->
+		<!--				{$_('home.quote.company')}-->
+		<!--			</div>-->
+		<!--		</div>-->
 	</article>
 </section>
 
