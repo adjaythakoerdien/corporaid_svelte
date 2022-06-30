ed6156a5f0f84d7a2cfd69b775ba4d283b860231
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-06-30 17:34:36 +0200

    Small updates

--- src/lib/home/home-vraag.svelte
+++ src/lib/home/home-vraag.svelte
@@ -10,23 +10,23 @@
 		</p>
 		<div class="row home-vraag-icons">
 			<a href="tel:+31612345678">
-				<div class="col s12 m4 home-vraag-telephone hoverable">
+				<div class="col s12 m6 home-vraag-telephone hoverable">
 					<i class="material-icons contact-icon" style="font-size:50px;">phone</i>
 					<p class="contact-text">06-1486 5297</p>
 				</div>
 			</a>
 			<a href="mailto:info@corporaid.fake">
-				<div class="col s12 m4 home-vraag-email hoverable">
+				<div class="col s12 m6 home-vraag-email hoverable">
 					<i class="material-icons contact-icon" style="font-size:50px;">mail</i>
 					<p class="contact-text">info@corporaid.nl</p>
 				</div>
 			</a>
-			<a href="/contact">
-				<div class="col s12 m4 hoverable contact-card">
-					<i class="material-icons contact-icon" style="font-size:50px;">contact_page</i>
-					<p class="contact-text">{$_('home.question.page')}</p>
-				</div>
-			</a>
+			<!--			<a href="mailto:info@corporaid.nl">-->
+			<!--				<div class="col s12 m4 hoverable contact-card">-->
+			<!--					<i class="material-icons contact-icon" style="font-size:50px;">contact_page</i>-->
+			<!--					<p class="contact-text">{$_('home.question.page')}</p>-->
+			<!--				</div>-->
+			<!--			</a>-->
 			<div class="col m2" />
 		</div>
 	</article>
