d8320351afa92f94f39036104f8c5f60d1b89d5f
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-07-02 20:53:30 +0200

    Updated MVO text

--- src/lib/services/services-section-sociaal.svelte
+++ src/lib/services/services-section-sociaal.svelte
@@ -13,8 +13,15 @@
 				<i class="material-icons" style="font-size:50px;color: #566BB3">groups</i>
 				<h5>{$_('services.socialSection.title')}</h5>
 				<p class="services-column-text">
+					{$_('services.socialSection.subtitle')}<br /><br />
 					{$_('services.socialSection.text')}
 				</p>
+				<ul>
+					<li>- {$_('services.socialSection.list1')}</li>
+					<li>- {$_('services.socialSection.list2')}</li>
+					<li>- {$_('services.socialSection.list3')}</li>
+					<li>- {$_('services.socialSection.list4')}</li>
+				</ul>
 				<a
 					href="/contact"
 					class="waves-effect waves-effect waves-light btn hoverable deep-orange accent-2"
