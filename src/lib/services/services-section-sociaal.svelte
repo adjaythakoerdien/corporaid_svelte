212801725f89507e2e271fc124be306cf4064da9
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-07-02 16:31:27 +0200

    Fixed MVO image

--- src/lib/services/services-section-sociaal.svelte
+++ src/lib/services/services-section-sociaal.svelte
@@ -1,15 +1,17 @@
 <script>
-	import SociaalImage from '$lib/assets/sociaal.webp';
 	import { _ } from 'svelte-i18n';
 </script>
 
 <section class="services-section">
-	<article class="container services-container">
+	<article class="container services-container finance">
 		<div class="row" role="presentation">
+			<div class="col s12 l6 services-column finance-photo hide-on-large-only" role="presentation">
+				<div class="services-sociaal-image" role="image" />
+			</div>
+
 			<div class="col s12 l5 services-column" role="presentation">
 				<i class="material-icons" style="font-size:50px;color: #566BB3">groups</i>
 				<h5>{$_('services.socialSection.title')}</h5>
-				<p>{$_('services.socialSection.subtitle')}</p>
 				<p class="services-column-text">
 					{$_('services.socialSection.text')}
 				</p>
@@ -22,13 +24,16 @@
 
 			<div class="col l1" role="presentation" />
 
-			<div class="col s12 l6 center services-column" role="image">
-				<img class="responsive-img" src={SociaalImage} alt="" />
+			<div
+				class="col s12 l6 services-column finance-photo hide-on-med-and-down"
+				role="presentation"
+			>
+				<div class="services-sociaal-image" role="image" />
 			</div>
 		</div>
 	</article>
 </section>
 
 <style>
-	/*@import 'services-section-sociaal.css';*/
+	@import 'services-section-sociaal.css';
 </style>
