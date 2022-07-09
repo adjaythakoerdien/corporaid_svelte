c9ff63e2f248619cfefc9835a2a59d669d6465d5
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-07-09 15:25:07 +0200

    Updated text for "onze diensten"

--- src/lib/layout/footer.svelte
+++ src/lib/layout/footer.svelte
@@ -13,7 +13,7 @@
 				<h5 class="white-text">Contact</h5>
 				<ul class="footer-contact">
 					<li><a class="grey-text text-lighten-3" href="#!">info@corporaid.nl</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">06-1486 5297</a></li>
+					<li><a class="grey-text text-lighten-3" href="#!">+31 (0)6 1486 5297</a></li>
 					<!--					<li><a class="grey-text text-lighten-3" href="#!">Ergens In Uithoorn 101</a></li>-->
 					<!--					<li><a class="grey-text text-lighten-3" href="#!">Uithoorn</a></li>-->
 				</ul>
--- src/lib/services/services-section-finance.svelte
+++ src/lib/services/services-section-finance.svelte
@@ -14,9 +14,81 @@
 			<div class="col s12 l5 services-column" role="presentation">
 				<i class="material-icons" style="font-size:50px;color: #566BB3">euro</i>
 				<h5>{$_('services.financeSection.title')}</h5>
-				<p class="services-column-text">
-					{$_('services.financeSection.text')}
-				</p>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">A</div>
+					<div>
+						{$_('services.financeSection.textA')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">B</div>
+					<div>
+						{$_('services.financeSection.textB')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">1</div>
+					<div>
+						{$_('services.financeSection.textB1')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">2</div>
+					<div>
+						{$_('services.financeSection.textB2')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">C</div>
+					<div>
+						{$_('services.financeSection.textC')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">D</div>
+					<div>
+						{$_('services.financeSection.textD')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">1</div>
+					<div>
+						{$_('services.financeSection.textD1')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">2</div>
+					<div>
+						{$_('services.financeSection.textD2')}
+					</div>
+				</div>
+				<div class="services-column-text" style="display:flex">
+					<div class="letter">3</div>
+					<div>
+						{$_('services.financeSection.textD3')}
+					</div>
+				</div>
+				<!--	{$_('services.financeSection.textB')}-->
+				<!--	{$_('services.financeSection.textB1')}-->
+				<!--	{$_('services.financeSection.textB2')}-->
+				<!--	{$_('services.financeSection.textC')}-->
+				<!--	{$_('services.financeSection.textD')}-->
+				<!--	{$_('services.financeSection.textD1')}-->
+				<!--	{$_('services.financeSection.textD2')}-->
+				<!--	{$_('services.financeSection.textD3')}-->
+				<!--</div>-->
+
+				<!--				<ul class="services-column-text">-->
+				<!--					<li>{$_('services.financeSection.textA')}</li>-->
+				<!--					<li>{$_('services.financeSection.textB')}</li>-->
+				<!--					<li>{$_('services.financeSection.textB1')}</li>-->
+				<!--					<li>{$_('services.financeSection.textB2')}</li>-->
+				<!--					<li>{$_('services.financeSection.textC')}</li>-->
+				<!--					<li>{$_('services.financeSection.textD')}</li>-->
+				<!--					<li>{$_('services.financeSection.textD1')}</li>-->
+				<!--					<li>{$_('services.financeSection.textD2')}</li>-->
+				<!--					<li>{$_('services.financeSection.textD3')}</li>-->
+				<!--				</ul>-->
 				<a
 					href="/contact"
 					class="waves-effect waves-effect waves-light btn hoverable deep-orange accent-2"
