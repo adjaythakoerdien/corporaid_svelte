ca45f01532167700c9f5631db99374021a943d85
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-07-19 19:49:13 +0200

    Updated en lang sidenav

---
+++ src/routes/en.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { setupI18n } from '../services/i18n';
+	let localLang = '';
+
+	setupI18n({ withLocale: 'en' });
+	localStorage.setItem('localLang', 'en');
+	const hostname = location.origin;
+	console.log(hostname);
+	location.href = hostname;
+
+	console.log('en');
+</script>
