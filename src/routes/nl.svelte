2a8e683fcde8b87dcd2fc1b1188d1451c6aa10ab
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-07-19 19:46:05 +0200

    Updated lang sidenav

--- src/routes/nl.svelte
+++ src/routes/nl.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-	// location.reload = 'https://www.corporaid.nl';
 	import { setupI18n } from '../services/i18n';
+	let localLang = '';
+
+	setupI18n({ withLocale: 'nl' });
 	localStorage.setItem('localLang', 'nl');
-	location.reload();
+	const hostname = location.host;
+	console.log(hostname);
+	location.href = hostname;
+
 	console.log('nl');
 </script>
